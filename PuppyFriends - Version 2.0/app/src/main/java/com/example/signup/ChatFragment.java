4c6af258afa9ea6b82e1d51a21ab70d3b10be014
iamguno <65309959+iamguno@users.noreply.github.com>
2020-06-14 19:18:04 +0900

    Chat Activity
    Chat

---
+++ PuppyFriends - Version 2.0/app/src/main/java/com/example/signup/ChatFragment.java
@@ -0,0 +1,21 @@
+package com.example.signup;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+
+public class ChatFragment extends Fragment {
+
+    public ChatFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_chat, container, false);
+        getActivity().setTitle("Chat");
+        return view;
+    }
+
+}
